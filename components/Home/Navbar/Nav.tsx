48cd61b8c29b4a904ba73057daf4dd3a32fe7ad2
an0abi <nataliabyczkowska7@gmail.com>
2025-08-11 14:20:06 +0200

    nav and theme toggle button (next-theme)

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,11 +1,16 @@
 "use client";
+import ThemeToggler from "@/components/Helper/ThemeToggler";
 import { NavLinks } from "@/constant/constant";
 import Link from "next/link";
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { HiBars3BottomRight } from "react-icons/hi2";
 import { LuNetwork } from "react-icons/lu";
 
-const Nav = () => {
+type NavProps = {
+  openNav: () => void;
+};
+
+const Nav = ({ openNav }: NavProps) => {
   const [navBg, setNavBg] = useState(false);
 
   useEffect(() => {
@@ -24,14 +29,18 @@ const Nav = () => {
   }, []);
 
   return (
-    <div className={`transition-all ${navBg ? "bg-white shadow-md" : "fixed"} duration-200 h-[12vh] z-[10000] fixed w-full`}>
+    <div
+      className={`transition-all ${
+        navBg ? "bg-white shadow-md dark:bg-gray-900" : "fixed"
+      } duration-200 h-[12vh] z-[10000] fixed w-full`}
+    >
       <div className="flex items-center h-full justify-between w-[92%] mx-auto">
         <div className="flex items-center sm:space-x-20">
           <div className="flex items-center space-x-2">
-            <div className="w-10 h-10 bg-cyan-800 rounded-full flex items-center justify-center flex-col">
-              <LuNetwork className="w-5 h-5 text-white" />
+            <div className="w-10 h-10 bg-cyan-800 dark:bg-white rounded-full flex items-center justify-center flex-col">
+              <LuNetwork className="w-5 h-5 text-white dark:text-cyan-800" />
             </div>
-            <h1 className="text-cl hidden sm:block md:text-2xl text-cyan-800 font-bold">
+            <h1 className="text-cl hidden sm:block md:text-2xl text-cyan-800 dark:text-white font-bold">
               DevHire
             </h1>
           </div>
@@ -51,14 +60,17 @@ const Nav = () => {
           </div>
         </div>
         <div className="flex items-center space-x-4">
-          <button className="px-8 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-200 hover:bg-gray-300 transition-all duration-300">
+          <button className="px-8 py-2.5 text-xs sm:text-sm rounded-lg cursor-pointer bg-gray-200 hover:bg-gray-300 transition-all duration-300 dark:text-black">
             Login/Register
           </button>
           <button className="px-8 py-2.5 text-sm hidden sm:block rounded-lg cursor-pointer bg-cyan-700 hover:bg-cyan-600 transition-all duration-300 text-white">
             Job Post
           </button>
-          {/* --- Theme Toggle --- */}
-          <HiBars3BottomRight className="w-8 h-8 cursor-pointer text-black lg:hidden" />
+          <ThemeToggler />
+          <HiBars3BottomRight
+            onClick={openNav}
+            className="w-8 h-8 cursor-pointer text-black lg:hidden"
+          />
         </div>
       </div>
     </div>
--- components/Home/Navbar/ResponsiveNav.tsx
+++ components/Home/Navbar/ResponsiveNav.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Nav from "./Nav";
-// import MobileNav from "./MobileNav";
+import MobileNav from "./MobileNav";
 
 const ResponsiveNav = () => {
+  const [showNav, setShowNav] = useState(false);
+  const openNavHandler = () => setShowNav(true);
+  const closeNavHandler = () => setShowNav(false);
+
+
   return (
-    <div >
-      <Nav />
-      {/* <MobileNav /> */}
+    <div>
+      <Nav openNav={openNavHandler} />
+      <MobileNav closeNav={closeNavHandler} showNav={showNav} />
     </div>
   );
 };
